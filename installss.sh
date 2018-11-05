47279f4ab13abbda8d5ba73dc24cbcb683be5d99
ariwori <linweiqing2016@gmail.com>
2018-11-05 22:37:35 +0800

    push

--- installss.sh
+++ installss.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # 简单的用于在CentOS7系统上一键安装SS C语言版本服务端（Shadowsocks-libev）的脚本，搬瓦工的建议选择带bbr的系统，脚本默认开启tcp_fast_open
-# SSR已年久失修，而Shadowsocks-libev还一直有人维护，建议换用SS
+# SSR已年久失修，而Shadowsocks-libev（https://github.com/shadowsocks/shadowsocks-libev）还一直有人维护，建议换用SS
 # Run on ROOT
 # By Ariwori
 
